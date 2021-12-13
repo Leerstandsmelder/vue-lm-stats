3cae28d972fbf573cb2f2d22f9162a636c180b67
Sefux <sfuchs@projektkater.de>
2021-12-13 23:43:19 +0000

    Deploying to gh-pages from @ Leerstandsmelder/vue-lm-stats@2ab17f16e3151ebd0908574e8641027d28cd3155 🚀

---
+++ precache-manifest.1ea94f8215b179bbc73c042c0d346145.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc1d6e2c12731f8577d",
+    "url": "/vue-lm-stats/css/app.915d9534.css"
+  },
+  {
+    "revision": "1733f8198243966750cf",
+    "url": "/vue-lm-stats/css/chunk-vendors.448a81c9.css"
+  },
+  {
+    "revision": "9cacdc876e2049988fcab540c21738d5",
+    "url": "/vue-lm-stats/fonts/materialdesignicons-webfont.9cacdc87.eot"
+  },
+  {
+    "revision": "9d243c168a4f1c2cb3cec74884344de7",
+    "url": "/vue-lm-stats/fonts/materialdesignicons-webfont.9d243c16.woff2"
+  },
+  {
+    "revision": "a0711490bcd581b647329230b3e915cf",
+    "url": "/vue-lm-stats/fonts/materialdesignicons-webfont.a0711490.woff"
+  },
+  {
+    "revision": "b62641afc9ab487008e996a5c5865e56",
+    "url": "/vue-lm-stats/fonts/materialdesignicons-webfont.b62641af.ttf"
+  },
+  {
+    "revision": "b038130205ae9f5f37898fb7745e0cb4",
+    "url": "/vue-lm-stats/img/logo_cropped_alt.b0381302.png"
+  },
+  {
+    "revision": "ad188224f3523580814aea7d02de248b",
+    "url": "/vue-lm-stats/index.html"
+  },
+  {
+    "revision": "60a63856a2c36545d5b6",
+    "url": "/vue-lm-stats/js/about.baf7e2f2.js"
+  },
+  {
+    "revision": "0cc1d6e2c12731f8577d",
+    "url": "/vue-lm-stats/js/app.10a22f86.js"
+  },
+  {
+    "revision": "1733f8198243966750cf",
+    "url": "/vue-lm-stats/js/chunk-vendors.2b300e90.js"
+  },
+  {
+    "revision": "2b9379c9ce441a7fb2a35849e2297199",
+    "url": "/vue-lm-stats/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-lm-stats/robots.txt"
+  }
+]);
